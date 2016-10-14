8b0ab2acf6eb2e7fd73bd31255c6404fd40e237f
usuario <usuario@10.22.143.55>
2016-10-14 09:37:51 +0100

    primera version kata 3

---
+++ src/kata3/v1/Kata3V1.java
@@ -0,0 +1,9 @@
+package kata3.v1;
+
+public class Kata3V1 {
+
+    public static void main(String[] args) {
+        HistogramDisplay histo = new HistogramDisplay ();
+        histo.execute();
+    }
+}
