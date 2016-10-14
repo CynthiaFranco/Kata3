53b5f98d2f4991202ab4e85ebeb633b8b82c1349
usuario <usuario@10.22.143.55>
2016-10-14 10:05:12 +0100

    primera version kata 3

--- src/kata3/v1/Kata3V1.java
+++ src/kata3/v1/Kata3V1.java
@@ -3,7 +3,16 @@ package kata3.v1;
 public class Kata3V1 {
 
     public static void main(String[] args) {
-        HistogramDisplay histo = new HistogramDisplay ();
-        histo.execute();
+        Histogram<String> histogram = new Histogram<>();
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ull.es");
+        histogram.increment("ull.es");
+        histogram.increment("hotmail.com");
+        new HistogramDisplay(histogram).execute();
+        
     }
 }
